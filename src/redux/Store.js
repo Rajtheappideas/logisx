ede95124242827be24bc41d030230b7a88a6dc04
Raj Chavda <raj.theappideas@gmail.com>
2023-11-21 12:59:00 +0530

    miner changes

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -35,7 +35,7 @@ export const store = configureStore({
   reducer: { root: persisteRoot },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
-  devTools: process.env.NODE_ENV !== "production",
+  // devTools: process.env.NODE_ENV !== "production",
 });
 
 export const persistor = persistStore(store);
